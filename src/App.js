35450d16a01aa672ab2d5cb4df9d1e4e0f2c95d3
Genos2000 <paraskamble2000@gmail.com>
2020-11-01 20:45:03 +0530

    yeet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SignInSide from './pages/signin';
+import SignUp from './pages/signup';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <Router>
+		<Switch>
+			<Route path='/' exact>
+				<div className="App">
+					<header className="App-header">
+						<h1>Welcome</h1>
+					</header>
+				</div>
+			</Route>
+			<Route path='/signin' exact>
+				<SignInSide />
+			</Route>
+			<Route path='/signup' exact>
+				<SignUp />
+			</Route>
+			<Route path="*">
+				<div className="App">
+					<header className="App-header">
+						<h1>Bruh</h1>
+					</header>
+				</div>
+			</Route>
+		</Switch>
+	</Router>;
 }
 
 export default App;
