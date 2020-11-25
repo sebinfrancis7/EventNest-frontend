5774fd0e364dc0ad7bc377e09af159a504e97ca8
Genos2000 <paraskamble2000@gmail.com>
2020-11-25 12:11:24 +0530

    https env

--- src/pages/eventinfo.jsx
+++ src/pages/eventinfo.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import axios from 'axios';
 import { Card, Grid, StylesProvider, Button, CardHeader } from '@material-ui/core';
@@ -7,6 +7,7 @@ import '../sass/eventinfo.scss';
 import { CardMedia } from '@material-ui/core';
 import { CardContent } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
+import { Select } from '@material-ui/core';
 import ShareIcon from '@material-ui/icons/Share';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import FacebookIcon from '@material-ui/icons/Facebook';
@@ -15,7 +16,7 @@ import { useUserContext, UserContext } from '../userContext';
 import {
 	FacebookShareButton,
 	TwitterShareButton
-  } from "react-share";
+} from 'react-share';
 
 function loadScript(src) {
 	return new Promise((resolve) => {
@@ -35,6 +36,7 @@ function EventInfo(props) {
 	const [details, setDetails] = useState([]);
 	const [orderData, setOrder] = useState();
 	const [ loadRazor, setLoadRazor ] = useState(true);
+	const [ number, setNumber ] = useState(1);
 	const [user, setUser] = useContext(UserContext);
 	const { match: { params } } = props;
 	let history = useHistory();
@@ -67,11 +69,11 @@ function EventInfo(props) {
 					transactionid : response.razorpay_payment_id,
 					transactionamount :  orderData.amount,
 					eventId: params.event_id,
-					tickets: tickets
-				}
+					tickets: number
+				};
 				axios.post('https://eventnest-server.herokuapp.com/razorpay/payment',values, { withCredentials : true })
-				.then(res=>{alert("Success")})
-				.catch(e=>console.log(e))
+					.then(res=>{alert('Success');})
+					.catch(e=>console.log(e));
 			},
 			// prefill: {
 			// 	name,
@@ -83,6 +85,11 @@ function EventInfo(props) {
 		paymentObject.open();
 	}
 	
+	const handleNumber = (event) => {
+		const number = event.target.value;
+		setNumber(number);
+	};	
+
 	useEffect(() => {
 		let url = 'https://eventnest-server.herokuapp.com/events?_id=' + params.event_id;
 		axios
@@ -95,17 +102,18 @@ function EventInfo(props) {
 
 	useEffect(() => {
 		const postBody = {
-			amount: details.price 
+			amount: details.price,
+			tickets: number
 		};
 		if(details.price)
 		{ 
 			axios
-				.post('https://eventnest-server.herokuapp.com/razorpay', postBody)
+				.post('http://localhost:4000/razorpay', postBody)
 				.then(res => {
 					setOrder(res.data);
 				});
 		}
-	}, [details]);
+	}, [number]);
 
 	return (
 		<StylesProvider injectFirst>
@@ -143,6 +151,21 @@ function EventInfo(props) {
 										</Typography>
 									</Grid>
 									<Grid className="event-button-container" item sm={6} xs={12}>
+										<Select
+											inputProps={{
+												name: 'Tickets'
+											}}
+											native
+											onChange={handleNumber}
+											value={number}
+										>
+											<option value={1}>1</option>
+											<option value={2}>2</option>
+											<option value={3}>3</option>
+											<option value={4}>4</option>
+											<option value={5}>5</option>
+											<option value={6}>6</option>
+										</Select>
 										<Button
 											className="event-register-button button-shadow"
 											color="primary"
@@ -172,15 +195,17 @@ function EventInfo(props) {
 							/>
 							<CardContent>
 								<FacebookShareButton 
-									url={"https://www.facebook.com/EventNest-102276125061269"}
-									quote={"Your online destination for hosting"}
-									hashtag={"#"+ details.title}>
+									hashtag={'#'+ details.title}
+									quote={'Your online destination for hosting'}
+									url={'https://www.facebook.com/EventNest-102276125061269'}
+								>
 									<FacebookIcon />
 								</FacebookShareButton>
 								<TwitterShareButton 
-									title={"Hey checkout this event on EventNest " + details.title}
+									hashtag={['#'+ details.title, '#eventnest']}
+									title={'Hey checkout this event on EventNest ' + details.title}
 									url={window.location.href}
-									hashtag={["#"+ details.title, "#eventnest"]}>
+								>
 									<TwitterIcon />
 								</TwitterShareButton>
 							</CardContent>
