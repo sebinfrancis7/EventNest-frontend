cf7eb7f2dcefe1b24fae487f1f42901da7d73e49
Diablo-web <67695053+Diablo-web@users.noreply.github.com>
2020-12-01 00:15:38 +0530

    Enter

--- src/components/EventPanel.jsx
+++ src/components/EventPanel.jsx
@@ -16,11 +16,24 @@ function EventPanel() {
 				/>
 			</div>
 			<div>
-				<h2 className='events-type'>Comedy</h2>
+				<h2 className='events-type'>COMEDY</h2>
 				<CardRow 
 					category={'comedy'}
 				/>
 			</div>
+			<div>
+				<h2 className='events-type'>MAGIC</h2>
+				<CardRow 
+					category={'Magic'}
+				/>
+			</div>
+			<div>
+				<h2 className='events-type'>CAMPING</h2>
+				<CardRow 
+					category={'camping'}
+				/>
+			</div>
+
 			<div>
 				<h2 className='events-type'>RECOMMENDED</h2>
 				<CardRow />
